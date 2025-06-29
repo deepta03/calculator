4282fe9a6b4360406e6d1a0a2d8b615906763309
Deepta <deepta370@gmail.com>
2025-06-29 20:44:32 +0600

    equals now stores the current sum for next operator and divison by zero returns ERROR

--- script.js
+++ script.js
@@ -16,21 +16,89 @@ let multiply = function (num1, num2){
 
 let divide = function(num1, num2){
 
+    if (num2==0) return "ERROR"
+
     return num1/num2;
 }
 
 let num1;
 let num2;
+let sum;
 let operator;
 
 let operate = function (num1,num2, operator){
 
    if (operator==="+") return add(num1, num2);
    if (operator ==="-") return subtract(num1, num2);
-   if (operator ==="*") return multiply(num1, num2);
-   if (operator ==="/") return divide(num1, num2);
+   if (operator ==="x") return multiply(num1, num2);
+   if (operator ==="÷") return divide(num1, num2);
 }
 
 
 let display= document.querySelector("p");
-display.innerHTML="1000";
+
+
+
+
+let numbers=document.querySelectorAll(".number")
+
+for(let i=0; i<numbers.length; i++){
+
+    numbers[i].addEventListener("click", () =>{
+
+        if(display.textContent==="0") display.textContent="";
+       
+
+    
+        display.textContent+=numbers[i].textContent;
+        
+        if(num2===undefined) num1=parseFloat(display.textContent);
+        else num2=parseFloat(display.textContent);
+
+    
+       
+        
+    })
+}
+
+
+
+
+
+let operators=document.querySelectorAll(".operator")
+
+for(let i=0; i<operators.length; i++){
+
+    operators[i].addEventListener("click", () =>{
+
+        display.textContent="";
+        num2=display.textContent;
+
+        if(sum) num1=sum;
+    
+        operator=operators[i].textContent;
+
+        
+
+    })
+}
+
+
+let equals=document.querySelector(".equals");
+
+equals.addEventListener("click", () => {
+
+    
+    
+    sum=operate(num1, num2, operator);
+
+    if(sum) display.textContent= sum;
+    
+    
+    
+    
+   
+})
+
+
+
